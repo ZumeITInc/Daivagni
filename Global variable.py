cda17b99beb651ddfe50d9555f2fae4fbee1992d
ZumeITInc <63601118+ZumeITInc@users.noreply.github.com>
2020-04-21 02:36:57 +0530

    Add files via upload

---
+++ Global variable.py
@@ -0,0 +1,30 @@
+# Defining global variable
+a=8
+def fun1():
+    print("waiting for time")
+    print(a)
+print(a)
+fun1()
+print(a)
+
+
+a=100
+def function1():
+    print("nothing is permanent")
+    print(a)
+    b=100
+    print(b)
+print(a)
+function1()
+print(b)
+
+
+
+
+a=564
+def gun():
+    a="Python"
+    print(a)
+print(a)
+gun()
+print(a)
