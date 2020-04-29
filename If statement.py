fc0bcc6c0a071a0121cbdce8e436083999ffcf8f
ZumeITInc <63601118+ZumeITInc@users.noreply.github.com>
2020-04-30 01:09:15 +0530

    Add files via upload

--- If statement.py
+++ If statement.py
@@ -1,14 +1,18 @@
 # 11 wap input total marks
-sname=input("student name:")
-roll_no=int(input("roll no:"))
+stud_name = input("Student name:")
+roll_no = int(input("roll no:"))
 s1=int(input("telugu:"))
 s2=int(input("hindi:"))
 s3=int(input("english:"))
 s4=int(input("maths:"))
 s5=int(input("science:"))
 s6=int(input("social:"))
-tm=s1+s2+s3+s4+s5+s6
-if tm>360:
+
+
+tm = s1+s2+s3+s4+s5+s6
+
+# applying if condition
+if tm > 360:
     print("1st class")
 if tm>=300 and tm<360:
     print("second class")
@@ -16,5 +20,16 @@ if tm<300:
     print("Third class")
 if tm<210:
     print("you are fail the exam")
-print("thankyou")
+print("Thankyou")
+
+
+#given two int  values,return their sum.unless the two values are the same,then return double their sum
+a=int(input("1stno"))
+b=int(input("2nd no"))
+sum=a+b
+if a==b:
+    print("sum=",sum+sum)
+else:
+    print(sum)
+print("thanks")
 
