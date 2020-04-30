bcd162271156aab89905e4614d3106296d3997fd
ZumeITInc <63601118+ZumeITInc@users.noreply.github.com>
2020-04-30 20:32:24 +0530

    Add files via upload

---
+++ List_one.py
@@ -0,0 +1,23 @@
+# program on list
+l1 = [110,27,75,48,42]
+print(l1[-1])
+l1[2] = 45
+l1[3] = 78
+l1[4] = 87
+print(l1)
+
+
+# example on using + operator on a list
+l1 = [17,22,75,42]
+l2 = [245,557,146,755]
+l3 = l1+l2
+print(l1)
+print(l2)
+print(l3)
+
+# example on multiple operator
+a1 = ["Python",10,False]
+a2 = a1 * 2
+print(a1)
+print(a2)
+
