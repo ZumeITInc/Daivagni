3c94171993cd27e3021196f5bc2badf1ed30fc4a
ZumeITInc <63601118+ZumeITInc@users.noreply.github.com>
2020-04-30 20:33:00 +0530

    Add files via upload

---
+++ List_two.py
@@ -0,0 +1,18 @@
+# using slice : operator on list
+l1=[14,25,37,411,570,750,1240,5,12,75]
+print(l1[0])
+print(l1[-1])
+print(l1[0:3])
+print(l1[0:9:2])
+print(l1[5:0:-1])
+
+# to print list in reverse order
+l1 = (l1[::-1])
+print(l1[-5:1:-1])
+
+# nested list
+l1 = [[10,20],[30,40,50],[60]]
+print(l1[0][0])
+print(l1[1][-1])
+print(l1[0])
+
